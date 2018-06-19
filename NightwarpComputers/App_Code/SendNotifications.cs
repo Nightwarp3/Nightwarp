ff6fb4e4f32b343ac18a23323af60d2700bb2898
Night Wrap <36970221+Nightwarp3@users.noreply.github.com>
2018-06-18 23:20:15 -0600

    Removed a password

--- NightwarpComputers/App_Code/SendNotifications.cs
+++ NightwarpComputers/App_Code/SendNotifications.cs
@@ -63,7 +63,7 @@ namespace NightwarpComputers.Content
         {
             SmtpClient client = new SmtpClient("smtp.office365.com", 587)
             {
-                Credentials = new NetworkCredential("NightwarpComputers@outlook.com", "BurpingBanshees2"),
+                Credentials = new NetworkCredential("NightwarpComputers@outlook.com", password),
                 EnableSsl = true
             };
 
