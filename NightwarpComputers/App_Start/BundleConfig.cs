2e469e65d833231f964ec9d902e84b9ac96b670d
Mike Campbell <swimmerboi93@hotmail.com>
2018-06-21 17:56:40 -0600

    added bootstrap.. needs lots of work.

--- NightwarpComputers/App_Start/BundleConfig.cs
+++ NightwarpComputers/App_Start/BundleConfig.cs
@@ -8,9 +8,9 @@ namespace NightwarpComputers
         // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new StyleBundle("~/Content/cssOld").Include("~/Content/oldSite.css"));
-
             bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/Site.css"));
+
+            bundles.Add(new StyleBundle("~/Content/bootstrap").Include("~/Content/bootstrap.min.css"));
 
             bundles.Add(new ScriptBundle("~/bundles/NightwarpComputers")
                 .IncludeDirectory("~/Scripts/Controllers", "*.js")
